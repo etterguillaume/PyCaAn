99549dca43266d3ba6220e52b09e8216665d8acf
Guillaume Etter <35970969+etterguillaume@users.noreply.github.com>
2022-05-16 17:39:39 -0400

    Added pipeline roadmap

--- main.py
+++ main.py
@@ -1,9 +1,43 @@
-#%% Argument parser
-
-# Run single files? overwrite? Population analysis?
+#%% Imports
+import yaml
+import os
+import pandas
+from scipy.io import loadmat
 
 #%% Load YAML file
+with open('params.yaml','r') as file:
+    params = yaml.full_load(file)
 
-#%%
+#%% Establish list of animals
+# Use pandas to create table of all recordings, input their date (use as ID). Row = mouse, column = experiment/session
+# Save in results, use this as list of sessions to process
 
+#%% Subject analysis
+# Load each matfile
+
+# Binarize calcium traces
+
+# Interpolate behavior
+
+# Compute velocities
+
+# Find periods of immobility, create an inclusion vector
+
+# Compute occupancy and joint probabilities
+
+# Compute tuning curves for every cell
+# [MI, PDF, occupancy_vector, prob_being_active, tuning_curve ] = extract_1D_information(binarized_trace, interp_behav_vec, bin_vector, inclusion_vector)
+
+# Smooth
+
+# Shuffle, compute significance
+
+# Compute split-half stability
+
+# Decoding (jacknife? Decoding using 1,2,4,8,16,32,64,128,256,512, ... max cells)
+
+# Save the data in a neat format!
+
+#%% Group analysis
+# Plot stuff!
 
