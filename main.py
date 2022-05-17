5d29ec39d0c82c66bd884daca9841be901b2b765
Guillaume Etter <35970969+etterguillaume@users.noreply.github.com>
2022-05-17 14:40:17 -0400

    interpolation, compute velocity

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import yaml
 import os
 import tqdm
 from functions.dataloaders import load_data
-from functions.signal_processing import binarize_ca_traces, interpolate_behavior
+from functions.signal_processing import binarize_ca_traces, interpolate_behavior, compute_velocity
 
 #%% Load YAML file
 print('Opening parameters file... ', end='')
@@ -37,14 +37,15 @@ print(f'Found {len(sessionsList)} total sessions')
 #%% Subject analysis
 #for session in tqdm.tqdm(sessionsList):
 data = load_data(params['path_to_dataset'] + os.sep + 'open_field' + os.sep + session)
-#%%
 binarized_traces = binarize_ca_traces(data['caTrace'], params['z_threshold'],params['sampling_frequency'])
-#%%
 interpolated_position = interpolate_behavior(data['position'], data['behavTime'], data['caTime'])
 
-    #velocity, running_ts = compute_velocity()
+#%%
+velocity, running_ts = compute_velocity(interpolated_position, data['caTime'], params['speed_threshold'])
 
 # Compute occupancy and joint probabilities
+
+
 
 # Compute tuning curves for every cell
 # [MI, PDF, occupancy_vector, prob_being_active, tuning_curve ] = extract_1D_information(binarized_trace, interp_behav_vec, bin_vector, inclusion_vector)
