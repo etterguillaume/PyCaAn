87ebe59f1cc33ef23fece3d9e189b955302dad59
Guillaume etter <etterguillaume@gmail.com>
2023-01-30 10:17:32 -0500

    Simplified function architecture

--- plot_summary.py
+++ plot_summary.py
@@ -8,9 +8,9 @@ import os
 import tqdm
 import matplotlib.pyplot as plt
 import numpy as np
-plt.style.use('plot_style.mplstyle')
-from functions.data_processing.dataloaders import load_data
-from functions.signal_processing.signal_processing import binarize_ca_traces, interpolate_behavior, compute_velocity
+from functions.dataloaders import load_data
+from functions.signal_processing import binarize_ca_traces, interpolate_behavior, compute_velocity
+from functions.plotting import plot_summary
 #%%
 with open('params.yaml','r') as file:
     params = yaml.full_load(file)
@@ -36,32 +36,5 @@ print(f'Found {len(sessionsList)} total sessions')
 data = load_data('/Users/guillaumeetter/Documents/datasets/calcium_imaging/open_field/M246/J20_246_lego005_20180621/')
 
 #%%
-plt.figure(figsize=(3,3))
-plt.subplot(221)
-plt.plot(data['position'][0],data['position'][1]) # Plot trajectory
-plt.title('Location (cm)')
-plt.xlim([0,params['open_field_width']])
-plt.ylim([0,params['open_field_width']])
-plt.xticks([0,25,50])
-plt.yticks([0,25,50])
-
-plt.subplot(222)
-plt.imshow(data['corrProj'],aspect='auto',cmap='bone')
-#plt.imshow(np.max(data['SFPs'],axis=0),aspect='auto',cmap='bone')
-plt.axis('off')
-
-plt.subplot(212)
-max_val=np.max(data['caTrace'])
-for i in range(params['max_ca_traces_to_plot']):
-    plt.plot(data['caTime'][0,:],data['caTrace'][i,:]*params['plot_gain']+max_val*i/params['plot_gain'], c=(1-i/50,.6,i/50))
-
-# %%
-data['numNeurons']
-# %%
-plt.plot(np.cos(np.arange(100)))
-
-# %%
-data['SFPs'].shape
-# %%
-data['numNeurons']
+plot_summary(data, params)
 # %%
