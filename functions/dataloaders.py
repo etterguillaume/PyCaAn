e59b047e66486467d303123c014adb7d34ad5c6e
Guillaume Etter <35970969+etterguillaume@users.noreply.github.com>
2023-02-24 11:47:27 -0500

    Updated dataloader, added seqLT state extract.

--- functions/dataloaders.py
+++ functions/dataloaders.py
@@ -9,12 +9,13 @@ def load_data(path):
         data.update(
                     {
                     'corrProj':np.array(f.get('ms/CorrProj')),
+                    'pnrProj': np.array(f.get('ms/PeakToNoiseProj')),
+                    'meanProj':np.array(f.get('ms/meanFrame')),
                     'experiment':np.array(f.get('ms/Experiment')),
+                    'date':f.get('ms/dateNum')[0][0],
                     'SFPs':np.array(f.get('ms/SFPs')),
-                    'frameNum':np.array(f.get('ms/frameNum')[0]),
-                    'numNeurons':int(np.array(f.get('ms/numNeurons'))[0]),
                     'caTime':np.array(f.get('ms/time'))[0]/1000, # convert ms->s
-                    'caTrace':np.array(f.get('ms/RawTraces')).T
+                    'rawData':np.array(f.get('ms/RawTraces')).T
                     }
                     )
     except: # If legacy MATLAB format
@@ -22,12 +23,13 @@ def load_data(path):
         data.update(
                     {
                     'corrProj':f['ms']['CorrProj'][0][0],
+                    'pnrProj': f['ms']['PeakToNoiseProj'][0][0],
+                    'meanProj': f['ms']['meanFrame'][0][0],
                     'experiment':f['ms']['Experiment'][0][0],
+                    'date':f['ms']['dateNum'][0][0],
                     'SFPs':f['ms']['SFPs'][0][0],
-                    'frameNum':f['ms']['frameNum'][0][0],
-                    'numNeurons':int(f['ms']['numNeurons'][0][0]),
                     'caTime':f['ms']['time'][0][0]/1000, # convert ms->s
-                    'caTrace':f['ms']['RawTraces'][0][0] 
+                    'rawData':f['ms']['RawTraces'][0][0] 
                     }
                     )
 
@@ -35,6 +37,8 @@ def load_data(path):
         f = h5py.File(path + '/behav.mat','r')
         data.update(
                     {
+                    'background':np.array(f.get('behav/background')),
+                    'tone':np.array(f.get('behav/optosignal')),
                     'position':np.array(f.get('behav/position')).T,
                     'behavTime':np.array(f.get('behav/time'))[0]/1000, # convert ms->s
                     'mazeWidth_px':np.array(f.get('behav/width'))[0][0],
@@ -44,7 +48,7 @@ def load_data(path):
     except:
         f = sio.loadmat(path + '/behav.mat')
         data.update(
-                    {
+                    { # Note that older files do not have background/tones
                     'position':f['behav']['position'][0][0],
                     'behavTime':f['behav']['time'][0][0].T[0]/1000,
                     'mazeWidth_px':f['behav']['width'][0][0],
--- functions/signal_processing.py
+++ functions/signal_processing.py
@@ -57,3 +57,89 @@ def preprocess_data(data, params):
                                              )
 
     return data
+
+def extract_tone(data, params):
+    threshold = 0.2
+    Fs = params['sampling_frequency']
+    tone = data['tone']
+
+    # Init vectors 
+    binary_signal = np.zeros(len(tone))
+
+    binary_signal[tone>threshold]=1
+    onset_signal = diff(binary_signal)
+    onset_signal = np.append(onset_signal,0)
+    flash_ts = np.where(onset_signal==1)
+    flash_int = diff(flash_ts/Fs)
+    flash_int = np.append(flash_int,0)
+    flash_freq = 1/flash_int
+
+    # Create lists of transitions
+    blank_to_slow=[]
+    slow_to_fast=[]
+    fast_to_solid=[]
+
+    # First stim is obvious from deriv data
+    blank_to_slow.append(0)
+
+    for i in range(1,len(flash_freq)):
+        if flash_freq[i-1] < 4 & flash_freq[i] > 4:
+            blank_to_slow.append(i)
+        elif flash_freq[i-1] < 6 & flash_freq[i] > 6:
+            slow_to_fast.append[i]
+        elif flash_freq[i-1] > 6 & flash_freq[i] < 4:
+            fast_to_solid.append(i)
+
+
+    # Assumed drop in frequency corresponding to solid light
+    flash_ts = np.append(flash_ts,flash_ts[-1])
+    fast_to_solid = np.append(fast_to_solid, len(flash_ts))
+
+    # Initialize state writes
+    state = binary_signal*3+1
+    state2write = 1
+
+    frame_buffer = 10
+
+    ct=0
+
+    for i in range (len(state)-frame_buffer):
+        if (state2write == 1) & (ct<=len(blank_to_slow)) & (flash_ts[blank_to_slow[ct]]<i):
+            blank_to_slow.pop(0)
+        
+        if (state2write == 2) & (ct<=len(slow_to_fast)) & (flash_ts[slow_to_fast[ct]]<i):
+            slow_to_fast.pop(0)
+        
+        if (state2write == 3) & (ct<=len(fast_to_solid)) & (flash_ts[fast_to_solid[ct]]<i):
+            fast_to_solid.pop(0)
+        
+        # Re-order state-transitions
+        while (len(slow_to_fast)>=(ct+frame_buffer)) & (slow_to_fast[ct]<=(blank_to_slow[ct]+frame_buffer)):
+            slow_to_fast.pop(0)
+
+        while (len(fast_to_solid)>=(ct+frame_buffer)) & (fast_to_solid[ct]<=(blank_to_slow[ct]+frame_buffer)):
+            fast_to_solid.pop(0)
+
+        while (len(fast_to_solid)>=(ct+frame_buffer)) & (fast_to_solid[ct]<=(slow_to_fast[ct]+frame_buffer)):
+            fast_to_solid.pop(0)
+
+    
+        # Detect state transitions
+    if (state2write==1) & (ct<=len(blank_to_slow)) & (flash_ts(blank_to_slow[ct])==i):
+        state2write = 2
+    elif (state2write==2) & (ct<=len(slow_to_fast)) & (flash_ts(slow_to_fast[ct])==i):
+        state2write = 3
+    elif (state2write==3) & (ct<=len(fast_to_solid)) & (flash_ts(fast_to_solid[ct])==i):
+        state2write = 4
+    elif (state2write==4) & (state(i)==1):
+        state2write = 1
+        if (ct+1<=len(blank_to_slow)) & (ct+1<=len(slow_to_fast)) & (ct+1<=len(fast_to_solid)):
+            ct+=1
+            while (flash_ts(blank_to_slow[ct]) < i):
+                blank_to_slow.pop(0)
+
+    state[i]=state2write
+
+    data['seqLT_state'] = state
+
+    return data
