82d4d65f97dea02996d1c9d4c7232fe9ad27e75a
Guillaume etter <etterguillaume@gmail.com>
2023-05-08 13:10:16 -0400

    Initiated RWI metric

--- functions/tuning.py
+++ functions/tuning.py
@@ -146,35 +146,19 @@ def extract_discrete_tuning(binaryData, interpolated_var, inclusion_ts, var_leng
     tuning_curve = active_frames_in_bin/occupancy_frames # Likelihood = number of active frames in bin/occupancy
     return AMI, p_value, occupancy_frames, active_frames_in_bin, tuning_curve
 
-def extract_internal_ratio(binaryData, interpolated_var, inclusion_ts, var_length, bin_size):
-    # Representational weight index
-    # X_bin_vector = np.arange(0,var_length+bin_size,bin_size)
-    # binaryData = binaryData[inclusion_ts]
-    # interpolated_var = interpolated_var[inclusion_ts]
-    # numFrames, numNeurons = binaryData.shape
-    # active_frames_in_bin = np.zeros((numNeurons,len(X_bin_vector)-1), dtype=int)
-    # occupancy_frames = np.zeros(len(X_bin_vector)-1, dtype=int)
-    # AMI = np.zeros(numNeurons)
-    # p_value = np.zeros(numNeurons)
+def extract_internal_info(data, params, inclusion_ts):
+    # Extract internal info
+        # Create internal bin tensor: retro_time, retro_dist, pro_time, pro_dist, speed
 
-    # # Compute occupancy
-    # bin_vector = np.zeros(numFrames, dtype=int) # Vector that will specificy the bin# for each frame
-    # ct=0
-    # for x in range(len(X_bin_vector)-1):
-    #     frames_in_bin = (interpolated_var >= X_bin_vector[x]) & (interpolated_var < X_bin_vector[x+1])
-    #     occupancy_frames[x] = np.sum(frames_in_bin) # How many frames for that bin
-    #     bin_vector[frames_in_bin] = ct
-    #     ct+=1
 
-    # # Bin activity
-    # for neuron in range(numNeurons):
-    #     for x in range(len(X_bin_vector)-1):
-    #         frames_in_bin = (interpolated_var >= X_bin_vector[x]) & (interpolated_var < X_bin_vector[x+1])
-    #         if frames_in_bin is not None: # if bin has been explored
-    #             active_frames_in_bin[neuron,x] = np.sum(binaryData[frames_in_bin,neuron]) # Total number of frames of activity in that bin
 
-    #     AMI[neuron] = adjusted_mutual_info_score(binaryData[:,neuron],bin_vector, average_method='min')
-    #     p_value[neuron] = chi2(binaryData[:,neuron][:,None],bin_vector[:,None])[1]
-    
-    # tuning_curve = active_frames_in_bin/occupancy_frames # Likelihood = number of active frames in bin/occupancy
-    return RWI # Representational weight index
+
+    return internal_info, p_value
+
+
+def extract_RWI(external_info, internal_info):
+    # Internal info = place field info
+    RWI = (external_info-internal_info)/(external_info+internal_info)
+
+    return RWI
+# Representational weight index (RWI)
