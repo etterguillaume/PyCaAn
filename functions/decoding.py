69dcb75b5067228a5437f1e6137aa6356886a41f
Guillaume etter <etterguillaume@gmail.com>
2023-06-05 18:03:02 -0400

    Added decoding script

--- functions/decoding.py
+++ functions/decoding.py
@@ -4,6 +4,55 @@ from sklearn.neighbors import KNeighborsClassifier as knn_class
 from sklearn.metrics import median_absolute_error as MAE
 #from sklearn.metrics import f1_score
 
+def decode_neural_data(var2predict, neural_data, params, trainingFrames, testingFrames):
+    np.random.seed(params['seed'])
+    prediction_stats = np.zeros(len(params['num_k']))*np.nan
+    error_stats = np.zeros(len(params['num_k']))*np.nan
+    for i, num_k  in enumerate(params['num_k']):
+        if var2predict.dtype=='float': # Use kNN regressor
+            decoder = knn_reg(metric='euclidean', n_neighbors=num_k).fit(neural_data[trainingFrames], var2predict[trainingFrames])
+            prediction = decoder.predict(neural_data[testingFrames])
+            error_stats[i] = MAE(var2predict[testingFrames],prediction)
+        else: # Use kNN classifier
+            decoder = knn_class(metric='euclidean', n_neighbors=num_k).fit(neural_data[trainingFrames], var2predict[trainingFrames])
+            #prediction = decoder.predict(test_embedding)
+            #error_stats[i] = np.nan # could use f1 score? But then opposite of error
+
+        prediction_stats[i] = decoder.score(neural_data[testingFrames], var2predict[testingFrames])
+
+    optimal_k = np.argmax(prediction_stats)
+    decoding_score = prediction_stats[optimal_k]
+    decoding_error = error_stats[optimal_k]
+
+    shuffled_score = np.zeros(params['num_surrogates'])
+    shuffled_error = np.zeros(params['num_surrogates'])
+    for shuffle_i in range(params['num_surrogates']):
+        idx = np.random.randint(len(neural_data))
+        shuffled_var = np.concatenate((var2predict[idx:], var2predict[:idx]))
+        prediction_stats = np.zeros(len(params['num_k']))*np.nan
+        error_stats = np.zeros(len(params['num_k']))*np.nan
+        for i, num_k  in enumerate(params['num_k']):
+            if var2predict.dtype=='float': # Use kNN regressor
+                decoder = knn_reg(metric='euclidean', n_neighbors=num_k).fit(neural_data[trainingFrames], shuffled_var[trainingFrames])
+                prediction = decoder.predict(neural_data[testingFrames])
+                error_stats[i] = MAE(var2predict[testingFrames],prediction)
+            else: # Use kNN classifier
+                decoder = knn_class(metric='euclidean', n_neighbors=num_k).fit(neural_data[trainingFrames], shuffled_var[trainingFrames])
+                #prediction = decoder.predict(test_embedding)
+                #error_stats[i] = np.nan # could use f1 score? But then opposite of error
+
+            prediction_stats[i] = decoder.score(neural_data[testingFrames],shuffled_var[testingFrames])
+
+        shuffled_score[shuffle_i] = np.max(prediction_stats)
+        optimal_k = np.argmax(prediction_stats)
+        shuffled_score[shuffle_i] = prediction_stats[optimal_k]
+        shuffled_error[shuffle_i] = error_stats[optimal_k]
+
+    decoding_zscore = (decoding_score-np.mean(shuffled_score))/np.std(shuffled_score)
+    decoding_pvalue = np.sum(shuffled_score>decoding_score)/params['num_surrogates']
+    shuffled_error = np.nanmean(shuffled_error)
+    return decoding_score, decoding_zscore, decoding_pvalue, decoding_error, shuffled_error
+
 def decode_embedding(var2predict,data, params, train_embedding, test_embedding):
     np.random.seed(params['seed'])
     prediction_stats = np.zeros(len(params['num_k']))*np.nan
