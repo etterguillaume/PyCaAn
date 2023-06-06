9dc1bc217e4d105fcdeed4a18c1532e0435bc6d6
Guillaume etter <etterguillaume@gmail.com>
2023-06-06 14:19:56 -0400

    Finalized decoding script

--- analysis/extract_decoding.py
+++ analysis/extract_decoding.py
@@ -61,7 +61,7 @@ for i, session in enumerate(tqdm(session_list)):
         with h5py.File(os.path.join(working_directory,'retrospective_decoding.h5'),'w') as f:
             
             decoding_score, z_score, p_value, decoding_error, shuffled_error = decode_neural_data(data['elapsed_time'],
-                                                                                                  data['binaryData'][:,0,params['input_neurons']],
+                                                                                                  data['binaryData'][:,0:params['input_neurons']],
                                                                                                   params,
                                                                                                   data['trainingFrames'],
                                                                                                   data['testingFrames'])
@@ -74,7 +74,11 @@ for i, session in enumerate(tqdm(session_list)):
 
     if not os.path.exists(os.path.join(working_directory,'prospective_temporal_decoding.h5')) or params['overwrite_mode']=='always':
         with h5py.File(os.path.join(working_directory,'prospective_temporal_decoding.h5'),'w') as f:
-            decoding_score, z_score, p_value, decoding_error, shuffled_error = decode_embedding(data['time2stop'],data, params, train_embedding, test_embedding)
+            decoding_score, z_score, p_value, decoding_error, shuffled_error = decode_neural_data(data['time2stop'],
+                                                                                                  data['binaryData'][:,0:params['input_neurons']],
+                                                                                                  params,
+                                                                                                  data['trainingFrames'],
+                                                                                                  data['testingFrames'])
             f.create_dataset('decoding_score', data=decoding_score)
             f.create_dataset('z_score', data=z_score)
             f.create_dataset('p_value', data=p_value)
@@ -84,7 +88,11 @@ for i, session in enumerate(tqdm(session_list)):
     # Decode distance travelled
     if not os.path.exists(os.path.join(working_directory,'retrospective_distance_decoding.h5')) or params['overwrite_mode']=='always':
         with h5py.File(os.path.join(working_directory,'retrospective_distance_decoding.h5'),'w') as f:
-            decoding_score, z_score, p_value, decoding_error, shuffled_error = decode_embedding(data['distance_travelled'],data, params, train_embedding, test_embedding)
+            decoding_score, z_score, p_value, decoding_error, shuffled_error = decode_neural_data(data['distance_travelled'],
+                                                                                                  data['binaryData'][:,0:params['input_neurons']],
+                                                                                                  params,
+                                                                                                  data['trainingFrames'],
+                                                                                                  data['testingFrames'])
             f.create_dataset('decoding_score', data=decoding_score)
             f.create_dataset('z_score', data=z_score)
             f.create_dataset('p_value', data=p_value)
@@ -93,7 +101,11 @@ for i, session in enumerate(tqdm(session_list)):
 
     if not os.path.exists(os.path.join(working_directory,'prospective_distance_decoding.h5')) or params['overwrite_mode']=='always':
         with h5py.File(os.path.join(working_directory,'prospective_distance_decoding.h5'),'w') as f:
-            decoding_score, z_score, p_value, decoding_error, shuffled_error = decode_embedding(data['distance2stop'],data, params, train_embedding, test_embedding)
+            decoding_score, z_score, p_value, decoding_error, shuffled_error = decode_neural_data(data['distance2stop'],
+                                                                                                  data['binaryData'][:,0:params['input_neurons']],
+                                                                                                  params,
+                                                                                                  data['trainingFrames'],
+                                                                                                  data['testingFrames'])
             f.create_dataset('decoding_score', data=decoding_score)
             f.create_dataset('z_score', data=z_score)
             f.create_dataset('p_value', data=p_value)
@@ -103,7 +115,11 @@ for i, session in enumerate(tqdm(session_list)):
     # Decode velocity
     if not os.path.exists(os.path.join(working_directory,'velocity_decoding.h5')) or params['overwrite_mode']=='always':
         with h5py.File(os.path.join(working_directory,'velocity_decoding.h5'),'w') as f:
-            decoding_score, z_score, p_value, decoding_error, shuffled_error = decode_embedding(data['velocity'],data, params, train_embedding, test_embedding)
+            decoding_score, z_score, p_value, decoding_error, shuffled_error = decode_neural_data(data['velocity'],
+                                                                                                  data['binaryData'][:,0:params['input_neurons']],
+                                                                                                  params,
+                                                                                                  data['trainingFrames'],
+                                                                                                  data['testingFrames'])
             f.create_dataset('decoding_score', data=decoding_score)
             f.create_dataset('z_score', data=z_score)
             f.create_dataset('p_value', data=p_value)
@@ -114,10 +130,18 @@ for i, session in enumerate(tqdm(session_list)):
     if not os.path.exists(os.path.join(working_directory,'spatial_decoding.h5')) or params['overwrite_mode']=='always':
         with h5py.File(os.path.join(working_directory,'spatial_decoding.h5'),'w') as f:
             if data['task']=='OF' or data['task']=='legoOF' or data['task']=='plexiOF':
-                decoding_score, z_score, p_value, decoding_error, shuffled_error = decode_embedding(data['position'],data, params, train_embedding, test_embedding)
+                decoding_score, z_score, p_value, decoding_error, shuffled_error = decode_neural_data(data['position'],
+                                                                                                  data['binaryData'][:,0:params['input_neurons']],
+                                                                                                  params,
+                                                                                                  data['trainingFrames'],
+                                                                                                  data['testingFrames'])
 
             elif data['task']=='LT' or data['task']=='legoLT' or data['task']=='legoToneLT' or data['task']=='legoSeqLT':
-                decoding_score, z_score, p_value, decoding_error, shuffled_error = decode_embedding(data['position'][:,0],data, params, train_embedding, test_embedding)
+                decoding_score, z_score, p_value, decoding_error, shuffled_error = decode_neural_data(data['position'][:,0],
+                                                                                                  data['binaryData'][:,0:params['input_neurons']],
+                                                                                                  params,
+                                                                                                  data['trainingFrames'],
+                                                                                                  data['testingFrames'])
 
             f.create_dataset('decoding_score', data=decoding_score)
             f.create_dataset('z_score', data=z_score)
@@ -130,11 +154,19 @@ for i, session in enumerate(tqdm(session_list)):
         if not os.path.exists(os.path.join(working_directory,'direction_decoding.h5')) or params['overwrite_mode']=='always':
             with h5py.File(os.path.join(working_directory,'direction_decoding.h5'),'w') as f:
                 if data['task'] == 'OF' or data['task'] == 'legoOF' or data['task'] == 'plexiOF':
-                    decoding_score, z_score, p_value, decoding_error, shuffled_error = decode_embedding(data['heading'],data, params, train_embedding, test_embedding)
+                    decoding_score, z_score, p_value, decoding_error, shuffled_error = decode_neural_data(data['heading'],
+                                                                                                  data['binaryData'][:,0:params['input_neurons']],
+                                                                                                  params,
+                                                                                                  data['trainingFrames'],
+                                                                                                  data['testingFrames'])
                     f.create_dataset('decoding_error', data=decoding_error)
                     f.create_dataset('shuffled_error', data=shuffled_error)
                 elif data['task'] == 'LT' or data['task'] == 'legoLT' or data['task'] == 'legoToneLT' or data['task'] == 'legoSeqLT':
-                    decoding_score, z_score, p_value = decode_embedding(data['LT_direction'],data, params, train_embedding, test_embedding)
+                    decoding_score, z_score, p_value, decoding_error, shuffled_error = decode_neural_data(data['LT_direction'],
+                                                                                                  data['binaryData'][:,0:params['input_neurons']],
+                                                                                                  params,
+                                                                                                  data['trainingFrames'],
+                                                                                                  data['testingFrames'])
             
                 f.create_dataset('decoding_score', data=decoding_score)
                 f.create_dataset('z_score', data=z_score)
@@ -149,7 +181,11 @@ for i, session in enumerate(tqdm(session_list)):
             if not os.path.exists(os.path.join(working_directory,'tone_decoding.h5')) or params['overwrite_mode']=='always':
                 with h5py.File(os.path.join(working_directory,'tone_decoding.h5'),'w') as f:
                     data=extract_tone(data,params)
-                    decoding_score, z_score, p_value = decode_embedding(data['binaryTone'],data, params, train_embedding, test_embedding)
+                    decoding_score, z_score, p_value, decoding_error, shuffled_error = decode_neural_data(data['binaryTone'],
+                                                                                                  data['binaryData'][:,0:params['input_neurons']],
+                                                                                                  params,
+                                                                                                  data['trainingFrames'],
+                                                                                                  data['testingFrames'])
             
             f.create_dataset('decoding_score', data=decoding_score)
             f.create_dataset('z_score', data=z_score)
@@ -162,7 +198,11 @@ for i, session in enumerate(tqdm(session_list)):
             if not os.path.exists(os.path.join(working_directory,'seqTone_decoding.h5')) or params['overwrite_mode']=='always':
                 with h5py.File(os.path.join(working_directory,'seqTone_decoding.h5'),'w') as f:
                     data = extract_seqLT_tone(data,params)
-                    decoding_score, z_score, p_value = decode_embedding(data['seqLT_state'],data, params, train_embedding, test_embedding)
+                    decoding_score, z_score, p_value, decoding_error, shuffled_error = decode_neural_data(data['seqLT_state'],
+                                                                                                  data['binaryData'][:,0:params['input_neurons']],
+                                                                                                  params,
+                                                                                                  data['trainingFrames'],
+                                                                                                  data['testingFrames'])
             
             f.create_dataset('decoding_score', data=decoding_score)
             f.create_dataset('z_score', data=z_score)
