e841d56310d8038307c0a929f402a8ee91179fb7
Guillaume etter <etterguillaume@gmail.com>
2023-06-29 10:07:34 -0400

    Updated setup descr

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(name='PyCaAn',
       version='.01',
-      description='Python Calcium imaging Analysis',
+      description='Python calcium imaging analysis (PyCaAn)',
       author='Guillaume Etter',
       author_email='etterguillaume@gmail.com',
       #url='',
