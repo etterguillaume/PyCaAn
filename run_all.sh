5dca5f71b4d6895238444102f9e7de5d200dc56b
Guillaume Etter <35970969+etterguillaume@users.noreply.github.com>
2023-07-03 23:22:04 -0400

    Minor changes

--- run_all.sh
+++ run_all.sh
@@ -2,7 +2,7 @@
 #pytest # First verify that everything works correctly
 
 ## Analyze dataset
-python3 run_dataset_curation.py # Prepare dataset
+#python3 run_dataset_curation.py # Prepare dataset
 python3 run_analysis.py --extract_basic_info --extract_tuning --extract_embedding --decode_embedding
 
 #python3 run_analysis.py --extract_basic_info
