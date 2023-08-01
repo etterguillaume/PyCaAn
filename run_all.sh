bc395588a94c0830fc7682a33a0b57af60f52ff5
Guillaume etter <etterguillaume@gmail.com>
2023-08-01 16:15:40 -0400

    Updated batch setup

--- run_all.sh
+++ run_all.sh
@@ -1,7 +1,12 @@
 ## Run tests
 #pytest # First verify that everything works correctly
 
-## Analyze dataset
+## Analyze dataset focusing on CA1 only
 python3 run_dataset_curation.py --param_file 'params_regions.yaml' # Prepare dataset
 python3 run_analysis.py --param_file 'params_regions.yaml' --extract_basic_info --plot_summary
-python3 run_analysis.py --param_file 'params_regions.yaml' --extract_tuning  --extract_embedding --extract_neural_structure --decode_embedding --align_embeddings
+python3 run_analysis.py --param_file 'params_regions.yaml' --extract_tuning  --extract_embedding --extract_neural_structure --decode_embedding
+
+## Analyze full dataset, with smaller number of neurons allowed
+python3 run_dataset_curation.py --param_file 'params_CA1.yaml' # Prepare dataset
+python3 run_analysis.py --param_file 'params_CA1.yaml' --extract_basic_info --plot_summary
+python3 run_analysis.py --param_file 'params_CA1.yaml' --extract_tuning  --extract_embedding --extract_neural_structure --decode_embedding --align_embeddings
