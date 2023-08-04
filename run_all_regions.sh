fb031fa9668c29df50643f1ff0ea2eeef73f113e
Guillaume Etter <35970969+etterguillaume@users.noreply.github.com>
2023-08-04 16:08:31 -0400

    minor update

--- run_all_regions.sh
+++ run_all_regions.sh
@@ -4,6 +4,6 @@
 ## Analyze dataset focusing on CA1 only
 #python3 run_dataset_curation.py --param_file 'params_regions.yaml' # Prepare dataset
 #python3 run_analysis.py --param_file 'params_regions.yaml' --extract_basic_info --plot_summary
-#python3 run_analysis.py --param_file 'params_regions.yaml' --extract_tuning  --extract_embedding --extract_neural_structure --decode_embedding --align_embeddings
-python3 run_analysis.py --param_file 'params_regions.yaml' --decode_embedding
+#python3 run_analysis.py --param_file 'params_regions.yaml' --extract_tuning  --extract_embedding --decode_embedding --align_embeddings
+python3 run_analysis.py --param_file 'params_regions.yaml' --extract_tuning
 #python3 pycaan/analysis/align_embeddings.py --param_file 'params_regions.yaml'
