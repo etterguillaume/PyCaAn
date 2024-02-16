d63b22a11654dd5aa3615ac8e76366305f4eb4b7
Guillaume Etter <etterguillaume@gmail.com>
2024-02-16 10:46:08 -0500

    Updated release version to conform format

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from distutils.core import setup
 
-setup(name='PyCaAn',
-      version='.01',
-      description='Python calcium imaging analysis (PyCaAn)',
-      author='Guillaume Etter',
-      author_email='etterguillaume@gmail.com',
-      #url='',
-      #packages=['numpy'],
-      py_modules=['functions','analysis']
-     )
+setup(
+    name="PyCaAn",
+    version="0.1.0",
+    description="Python calcium imaging analysis (PyCaAn)",
+    author="Guillaume Etter",
+    author_email="etterguillaume@gmail.com",
+    # url='',
+    # packages=['numpy'],
+    py_modules=["functions", "analysis"],
+)
+
