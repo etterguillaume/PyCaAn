a3105dbadb927ef9813e470078ed345268355a3e
Guillaume Etter <etterguillaume@gmail.com>
2024-07-16 09:48:10 -0400

    Added pyproject.toml

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="pycaan",
-    version="0.1.1",
+    version="0.1.2",
     description="Python calcium imaging analysis (PyCaAn)",
     author="Guillaume Etter",
     author_email="etterguillaume@gmail.com",
